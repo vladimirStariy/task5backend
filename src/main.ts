97cedf947fb221886063ea3f662fd6f8602874f1
vladimirStary <puffychords@gmail.com>
2023-10-16 22:08:44 +0300

    ads13

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import cors from 'cors';
 
 async function start() {
     const PORT = '5000';
     const app = await NestFactory.create(AppModule);
 
-    app.use(cors({
+    app.enableCors({
         origin: '*'
-    }))
+    });
 
     await app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 }
