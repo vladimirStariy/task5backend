a6e3e39093c06e455375e0d9accfcdafd4246c03
vladimirStary <puffychords@gmail.com>
2023-10-16 20:29:40 +0300

    ads8

--- src/fakeData/fake.module.ts
+++ src/fakeData/fake.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { FakeService } from './fake.service';
 import { FakeController } from './fake.controller';
+import { Faker } from '@faker-js/faker';
 
 @Module({
   controllers: [FakeController],
   providers: [FakeService],
   imports: [],
   exports: [
-    FakeService
+    FakeService,
+    Faker
   ]
 })
 
